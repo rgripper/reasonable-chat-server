5ef47e95057fee9dfa0398981308b4f5d3714fe1
Vladimir Zotov <azuresplash@gmail.com>
2019-01-08 08:40:51 +1100

    Forced websocket

--- src/SocketIOInterop.js
+++ src/SocketIOInterop.js
@@ -3,7 +3,7 @@ const io = require('socket.io');
 
 const httpServer = http.createServer((_, response) => response.end("Chat server is listening"));
 
-const socketServer = io(httpServer); //, { wsEngine: 'ws', transports: ['websocket'] });
+const socketServer = io(httpServer, { wsEngine: 'ws', transports: ['websocket'] });
 
 const startServer = (clientEventNames, serverEventName, initialSocketState, handler) => {
   const broadcast = (value) => socketServer.emit(serverEventName, value);
